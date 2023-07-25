5a0b5880077555c9374d4d2b23473a58384d50cb
8888q <littlejakers@gmail.com>
2023-07-24 22:59:53 -0400

    More bots, slightly prettier.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "chessground/assets/chessground.base.css";
 import "chessground/assets/chessground.brown.css";
 import "chessground/assets/chessground.cburnett.css";
 
-import {GenericBot, RandomBot, AlphabeticalBot} from './bots';
+import {RandomBot, AlphabeticalBot, ReverseAlphabeticalBot, GreedyBot, PrinciplesBot} from './bots';
 import { Select, MenuItem, InputLabel, FormControl, Box } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useState } from 'react';
@@ -16,17 +16,30 @@ const theme = createTheme({
 });
 
 function App() {
-  const [botName, setBot] = useState("random");
-  const botFuncDict = {"alphabetical": AlphabeticalBot, "random": RandomBot};
+  const [botName, setBot] = useState("alphabetical");
+  const botFuncDict = {"alphabetical": <AlphabeticalBot/>,
+                       "reverseAlphabetical": <ReverseAlphabeticalBot/>,
+                       "greedy": <GreedyBot/>,
+                       "principles": <PrinciplesBot/>,
+                       "random": <RandomBot/>
+                      };
   const botTextDict = {
     "alphabetical": 
         ["Alphabetical Bot",
-         "  When transcribing a chess game, there is a specific notation for each move. For example, moving a knight to the square a3 would look like 'Na3'. Does playing the first move alphabetically make for good play?"],
+         "When transcribing a chess game, there is a specific notation for each move. For example, moving a knight to the square a3 would look like 'Na3'. Does playing the first move alphabetically make for good play?"],
+    "reverseAlphabetical":
+        ["Reverse Alphabetical Bot",
+         "Same as Alphabetical Bot, but choose the move alphabetically last."],
+    "greedy":
+        ["Greedy Bot",
+         "This bot will always capture the most valuable piece it can. If it can't capture a piece, it will make a random move."],
+    "principles":
+        ["Principles Bot",
+         "When learning chess, beginners are often taught many principles to guide their decision making, such as 'Control the center', 'develop your pieces', etc. This bot attempts to follow chess principles according to the current position, but lacks foresight."],
     "random": 
         ["Random Bot",
-         "  This bot plays a random move out of all legal moves." ]
+         "This bot plays a random move out of all legal moves." ]
   }
-  const chessSize = Math.floor(window.innerHeight*0.7);
 
   const handleChange = (event) => {
     setBot(event.target.value);
@@ -37,7 +50,7 @@ function App() {
       <br/>
       <h1>Miscellaneous Chess Bots</h1>
       <div className='board'>
-        <GenericBot width={chessSize} height={chessSize} moveFunction={botFuncDict[botName]}/>
+        {botFuncDict[botName]}
       </div>
       <div className='InfoBox'>
         <div className='Select'>
@@ -52,6 +65,9 @@ function App() {
                   onChange={handleChange}
                   >
                   <MenuItem value={"alphabetical"}>AlphabeticalBot</MenuItem>
+                  <MenuItem value={"reverseAlphabetical"}>ReverseAlphabeticalBot</MenuItem>
+                  <MenuItem value={"greedy"}>GreedyBot</MenuItem>
+                  <MenuItem value={"principles"}>PrinciplesBot</MenuItem>
                   <MenuItem value={"random"}>RandomBot</MenuItem>
                 </Select>
               </FormControl>
@@ -59,7 +75,7 @@ function App() {
           </ThemeProvider>
         </div>
         <div className='Info'>
-          <p>{botTextDict[botName][0]}:</p>
+          <h3>{botTextDict[botName][0]}:</h3>
           <p>{botTextDict[botName][1]}</p>
         </div>
       </div>
--- src/bots.tsx
+++ src/bots.tsx
@@ -1,42 +1,167 @@
 import React, {useState} from 'react'
 
-import { Chess, Move, Square } from 'chess.js';
+import { Chess, Move, Square, DEFAULT_POSITION, PieceSymbol } from 'chess.js';
 import Chessground from "@react-chess/chessground";
 
 type BotProps = {
-    moveFunction: (board: Chess) => Move,
-    width?: number,
-    height?: number
+    moveFunction: (board: Chess) => string,
 }
 
-export function RandomBot(board: Chess) {
-    const moves: Move[] = board.moves({verbose: true});
-    const move: Move = moves[Math.floor(Math.random() * moves.length)];
-
-    return move;   
+export function AlphabeticalBot() {
+    function move(board: Chess): string {
+        let moves: Move[] = board.moves({verbose: true});
+        moves.sort(((a: Move, b: Move) => (a.san < b.san ? -1 : 1)));
+        const move: Move = moves[0];
+        
+        return move.san;    
+    }
+    
+    return (
+        <GenericBot moveFunction={move} />
+    );
 }
 
-export function AlphabeticalBot(board: Chess) {
-    let moves: Move[] = board.moves({verbose: true});
-    moves = moves.sort(((a: Move, b: Move) => (a.san < b.san ? -1 : 1)));
-    const move: Move = moves[0];
+export function ReverseAlphabeticalBot() {
+    function move(board: Chess): string {
+        let moves: Move[] = board.moves({verbose: true});
+        moves.sort(((a: Move, b: Move) => (a.san > b.san ? -1 : 1)));
+        const move: Move = moves[0];
+        
+        return move.san;
+    }
+    
+    return (
+        <GenericBot moveFunction={move} />
+    );
+}
 
-    return move;    
+export function GreedyBot(): JSX.Element {
+    function move(board: Chess): string {
+        let moves: Move[] = board.moves({verbose: true});
+        var candidates: Move[] = captures(moves);
+        if(candidates.length == 0) return moves[Math.floor(Math.random() * moves.length)].san;
+    
+        return candidates[0].san;   
+    }
+
+    return (
+        <GenericBot moveFunction={move} />
+    );
+}
+
+export function PrinciplesBot(): JSX.Element {
+    function move(board: Chess): string {
+        let moves: Move[] = board.moves({verbose: true});
+        let chess: Chess;
+        //deliver checkmate
+        for(let index in moves ){
+            chess = new Chess(moves[index].after);
+            if( chess.isCheckmate() ) return moves[index].san;
+        }
+        const caps = captures(moves);
+        //take [free] pieces
+        for( let index in caps ){
+            let capturedValue = PIECE_VALUE.get(caps[index].captured as PieceSymbol) ?? 1;
+            let capturingValue = PIECE_VALUE.get(caps[index].piece) ?? 1;
+            if(capturedValue >= capturingValue || !board.isAttacked(caps[index].to, 'w')) return caps[index].san;
+        }
+        //control the center
+        if( !board.isAttacked('e5','w')  && !board.get('e5') && board.get('e7').color == 'b' && board.get('e7').type == 'p' ) return 'e5';
+        else if( !board.isAttacked('d5','w') && !board.get('d5') && board.get('d7').color == 'b' && board.get('d7').type == 'p') return 'd5';
+        //develop your pieces, knights before bishops
+        if( board.get('g8').type == 'n' && board.get('g8').color == 'b' ){
+            if( !board.isAttacked('f6','w') && !board.get('f6')) return 'Nf6';
+        }
+        if( board.get('b8').type == 'n' && board.get('b8').color == 'b' ){
+            if( !board.isAttacked('c6','w') && !board.get('c6')) return 'Nc6';
+        }
+        if( board.get('f8').type == 'b' && board.get('f8').color == 'b' ){
+            if( !board.isAttacked('c5','w') && !board.get('c5') ) return 'Bc5';
+            else if( !board.isAttacked('d6','w') && !board.get('d6') ) return 'Bd6';
+            else if( !board.isAttacked('e7','w') && !board.get('e7') ) return 'Be7';
+        }
+        if( board.get('c8').type == 'b' && board.get('c8').color == 'b' ){
+            if( !board.isAttacked('f5','w') && !board.get('f5') ) return 'Bf5';
+            else if( !board.isAttacked('e6','w') && !board.get('e6') ) return 'Be6';
+            else if( !board.isAttacked('d7','w') && !board.get('d7') ) return 'Bd7';
+        }
+        //castle to safety
+        let castlingRights = board.getCastlingRights('b');
+        if( castlingRights.k && !board.get('f8') && !board.get('g8') && !board.isAttacked('f8', 'w') && !board.isAttacked('g8', 'w') && !board.isCheck()){
+            return "O-O";
+        }
+        else if( castlingRights.q && !board.get('d8') && !board.get('c8') && !board.get('b8') && !board.isAttacked('d8', 'w') && !board.isAttacked('c8', 'w') && !board.isCheck()){
+            return "O-O-O";
+        }
+        //don't hang your pieces
+        let candidates: Move[] = [];
+        for( let index in moves ){
+            if(!board.isAttacked(moves[index].to, 'w')) candidates.push(moves[index]);
+        }
+        if(candidates.length == 0) return moves[Math.floor(Math.random() * moves.length)].san;
+        return candidates[Math.floor(Math.random() * candidates.length)].san;
+        //moves.sort( ((a: Move, b: Move) => (chess.isAttacked(a.to, 'w') && !chess.isAttacked(b.to, 'w') ? 1 : -1)) )
+        //return moves[0].san;   
+    }
+
+    return (
+        <GenericBot moveFunction={move} />
+    );
+}
+
+export function RandomBot(): JSX.Element {
+    function move(board: Chess): string {
+        const moves: Move[] = board.moves({verbose: true});
+        const move: Move = moves[Math.floor(Math.random() * moves.length)];
+        console.log(move.san);
+    
+        return move.san;   
+    }
+
+    return (
+        <GenericBot moveFunction={move} />
+    );
 }
 
 export function GenericBot(props: BotProps): JSX.Element {
-    const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    const [fen, setFen] = useState(DEFAULT_POSITION)
+    const [isCheck, setCheck] = useState<boolean | "white" | "black">(false);
     const [dests, setDests] = useState(toDests(new Chess()));
+
+     const chessSize = Math.floor(window.innerHeight*0.7);
 
     function makeMove(orig: string, dest: string){
         let chess = new Chess(fen);
         if( chess.turn() === 'w' ){
             chess.move( {from: orig, to: dest} );
         }
-        if(chess.isCheckmate()) return;
-        const move: Move = props.moveFunction(chess);
-        chess.move(move.san);
-        if(chess.isCheckmate()) return;
+
+        function checkmate(color: boolean | "white" | "black"){
+            setCheck(color);
+            setFen(chess.fen());
+            setDests(new Map());
+            setTimeout(() => {
+                setCheck(false);
+                setFen(DEFAULT_POSITION);
+                setDests(toDests(new Chess()));
+            }, 2000);
+        }
+
+        if(chess.isCheckmate()) {
+            checkmate("black");
+            return;
+        } 
+        chess.move(props.moveFunction(chess));
+        if(chess.isCheckmate()){
+            checkmate("white");
+            return;
+        } 
+        if(chess.isCheck()){
+            setCheck("white");
+        } 
+        else{
+            setCheck(false);
+        }
         setFen(chess.fen());
         setDests(toDests(chess));
     }
@@ -44,11 +169,36 @@ export function GenericBot(props: BotProps): JSX.Element {
     return (
         <div>
             <Chessground
-                width={props.width}
-                height={props.height}
-                config={{fen: fen, events: {move: makeMove}, movable: {free: false, dests: dests}}}/>
+                width={chessSize}
+                height={chessSize}
+                config={
+                    {
+                        fen: fen,
+                        highlight: {
+                            lastMove: false,
+                            check: true
+                        },
+                        check: isCheck,
+                        events: {move: makeMove},
+                        movable: {free: false, dests: dests}
+                    }
+                }
+            />
         </div>
     )   
+}
+
+//returns all moves that are captures, sorted from most valuable to least valuable
+function captures(moves: Move[]): Move[]{
+        let candidates: Move[] = [];
+        for(let index in moves ){
+            if(moves[index].captured !== undefined){
+                candidates.push(moves[index]);
+            } 
+        }
+        candidates.sort(((a: Move, b: Move) => (PIECE_VALUE.get(a.captured as PieceSymbol) ?? 0 < (PIECE_VALUE.get(b.captured as PieceSymbol) ?? 0) ? -1 : 1)));
+        console.log(candidates);
+        return candidates;
 }
 
 function toDests(chess: Chess): Map<Square, Square[]> {
@@ -59,6 +209,16 @@ function toDests(chess: Chess): Map<Square, Square[]> {
     });
     return dests;
   }
+
+  const PIECE_VALUE: Map<PieceSymbol, number> = new Map(
+    [
+        ['p', 1],
+        ['b', 3],
+        ['n', 3],
+        ['r', 5],
+        ['q', 9]
+    ]
+  );
 
   const SQUARES: Square[] = [
     'a8',
